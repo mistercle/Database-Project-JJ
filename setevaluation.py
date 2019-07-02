cfe2d19b5654d6e5f615f56f2929456d55cf9474
mistercle <mistercle@gmail.com>
2019-07-02 14:04:32 +0900

    no message

--- setevaluation.py
+++ setevaluation.py
@@ -20,12 +20,12 @@ def evaluation(cnx, cursor):
         return
     elif len(result_assemblyman) > 1:
         print("*동명이인이 있습니다.")
-    for i, assemblyman in enumerate(result_assemblyman):
-        print("%d. %s %s %s" % (i+1, assemblyman[1], assemblyman[2], assemblyman[3]))
-    assemblymannum = input("\n평가할 국회위원 번호를 입력하세요 >> ")
-    if int(assemblymannum) > len(result_assemblyman):
-        print("*ERROR : 잘못된 번호입니다.")
-        return
+        for i, assemblyman in enumerate(result_assemblyman):
+            print("%d. %s %s %s" % (i+1, assemblyman[1], assemblyman[2], assemblyman[3]))
+        assemblymannum = input("\n평가할 국회위원 번호를 입력하세요 >> ")
+        if int(assemblymannum) > len(result_assemblyman):
+            print("*ERROR : 잘못된 번호입니다.")
+            return
     result_one = result_assemblyman[int(assemblymannum)-1]
     data_favor = int(input("\n어떻게 생각하시나요?(긍정적이라면 1, 부정적이라면 -1) >> "))
     data_content = input("\n그 이유를 입력하세요 >> ")
@@ -36,8 +36,6 @@ def evaluation(cnx, cursor):
 def initrep(cnx, cursor):
     insert_query = "UPDATE assemblyman set reputation = 0 where reputation is NULL"
     cursor.execute(insert_query)
-    cursor.execute("select reputation from assemblyman")
-    print(cursor.fetchall())
     cnx.commit()
 
 def setreptrigger(cnx, cursor):
